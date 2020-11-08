458e8e3fb374b9f06447153c8877f607351b32d6
Nate Weisiger <nateweisiger@gmail.com>
2020-11-08 16:48:26 -0700

    Route scaffolding

--- public/index.php
+++ public/index.php
@@ -5,11 +5,40 @@ use Slim\Factory\AppFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
+// Create App
 $app = AppFactory::create();
 
+/**
+ * Events UI
+ */
 $app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("Hello world!");
+    $response->getBody()->write("Events UI");
     return $response;
 });
 
+/**
+ * Events API - GET All
+ */
+$app->get('/api/events', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Events API - GET");
+    return $response;
+});
+
+/**
+ * Events API - GET by ID
+ */
+$app->get('/api/events/{id}', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Events API - GET by ID");
+    return $response;
+});
+
+/**
+ * Load seed data
+ */
+$app->get('/load_data', function (Request $request, Response $response, $args) {
+    $response->getBody()->write("Load seed data");
+    return $response;
+});
+
+// Run the app
 $app->run();
